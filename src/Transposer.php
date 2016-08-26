ceb6b5774137a6da06e1bb2d72600cb0da2a7cd4
Aldrin Jerome Almacin <aldrin@bluefinenterprises.com>
2016-08-26 15:07:34 -0400

    Initial commit

---
+++ src/Transposer.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Transposer
+ */
+class Transposer
+{
+    const NOTES = [ 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B' ];
+
+    public static function transpose($key)
+    {
+        return 'C D E F G A B';
+    }
+}
